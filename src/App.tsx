56836b4c44ffdd47a1a0b6468c77fdc925edeb62
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-02-22 14:09:08 -0300

    feat: Creating tags mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useQuery } from "@tanstack/react-query"
-import { FileDown, MoreHorizontal, Plus, Search } from "lucide-react"
+import { keepPreviousData, useQuery } from "@tanstack/react-query"
+import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react"
 import { Header } from "./components/header"
 import { Tabs } from "./components/tabs"
 import { Button } from "./components/ui/button"
 import { Control, Input } from "./components/ui/input"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table"
 import { Pagination } from "./components/pagination"
+import { useSearchParams } from "react-router-dom"
+import { useState } from "react"
+import * as Dialog from '@radix-ui/react-dialog'
+import { CreateTagForm } from "./components/create-tag-form"
 
 export interface TagResponse {
   first: number
@@ -18,26 +22,43 @@ export interface TagResponse {
 }
 
 export interface Tag {
+  id: string
   title: string
+  slug: string
   quantity: number
 }
 
 export function App() {
 
-  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
-    queryKey: ['get-tags'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/tags?_page=1&_per_page=10')
-      const data = await response.json()
+  const [searchParams, setSearchParams] = useSearchParams()
 
-      console.log(data)
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1
+  const urlFilter = searchParams.get('filter') ?? ""
+
+  const [filter, setFilter] = useState(urlFilter)
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', page],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
+      const data = await response.json()
 
       return data
     },
+    placeholderData: keepPreviousData,
   })
 
   if (isLoading) {
     return null
+  }
+
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', '1')
+      params.set('filter', filter)
+
+      return params
+    })
   }
 
   return (
@@ -49,16 +70,44 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary" >
-            <Plus className="size-3" />
-            Create new
-          </Button>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary" >
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className="fixed space-y-10 right-0 top-0 bottom-0 h-screen min-w-[320px] bg-zinc-950 border-l border-zinc-900 p-10">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create  tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts.
+                  </Dialog.Description>
+                </div>
+                <CreateTagForm />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </div>
         <div className="flex items-center justify-between">
-          <Input>
-            <Search className="size-3" />
-            <Control placeholder="Search tags..." />
-          </Input>
+          <div className="flex items-center">
+            <Input variant="filter">
+              <Search className="size-3" />
+              <Control
+                placeholder="Search tags..."
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+              />
+            </Input>
+            <Button onClick={handleFilter}>
+              <Filter className="size-3" />
+              Filter
+            </Button>
+          </div>
 
           <Button>
             <FileDown className="size-3" />
@@ -78,12 +127,12 @@ export function App() {
           <TableBody>
             {tagsResponse?.data.map(tag => {
               return (
-                <TableRow key={tag.title + tag.quantity}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
                       <span className="font-medium">{tag.title}</span>
-                      <span className="text-xs text-zinc-500">{tag.title + tag.quantity}</span>
+                      <span className="text-xs text-zinc-500">{tag.slug}</span>
                     </div>
                   </TableCell>
                   <TableCell className="text-zinc-300">
@@ -100,7 +149,7 @@ export function App() {
           </TableBody>
         </Table>
 
-        <Pagination  />
+        {tagsResponse && <Pagination items={tagsResponse.items} pages={tagsResponse.pages} page={page} />}
       </main>
     </div>
   )
