478d4822174704887a405aa025f4988cd4815cfd
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-02-23 02:49:41 -0300

    refactor: Adjusting css and creating new variant 'delete' in button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,11 +7,11 @@ const button = tv({
   variants: {
     variant: {
       default:
-        'py-1.5 px-2.5 rounded-md bg-zinc-900 border border-zinc-800 text-zinc-300 hover:border-zinc-700',
+        'py-1.5 px-2.5 rounded-md bg-zinc-900 border border-zinc-800 text-zinc-300 enabled:hover:border-zinc-700',
       primary:
-        'py-1 px-2 rounded-full bg-teal-400 text-teal-950 hover:bg-teal-500',
+        'py-1 px-2 rounded-full bg-teal-400 text-teal-950 enabled:hover:bg-teal-500',
       delete: 
-        'py-1.5 px-2.5 rounded-md border border-zinc-800 text-red-400',
+        'py-1.5 px-2.5 rounded-md border border-zinc-800 text-red-400 enabled:hover:border-zinc-700',
     },
     size: {
       default: '',
