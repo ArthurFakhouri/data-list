0126da32e63aa64bb22d9ee5d25dd0b663978284
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-02-23 02:48:35 -0300

    feat: Setting up sonner toast

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient()
 
@@ -17,6 +18,15 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster
+        toastOptions={{
+          unstyled: true,
+          classNames: {
+            success: 'flex items-center flex-wrap gap-1.5 bg-zinc-900 text-teal-400 rounded-xl p-5 w-[320px] max-w-[320px]'
+          },
+          duration: 3000
+        }}
+      />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
